9037b868e6e57de541cf829d0726691f3e757a2c
= <=>
2026-01-06 19:57:54 +0530

    Adding webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        static: './build',
+        port: 3000,
+        open: true,
+        hot: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+}
